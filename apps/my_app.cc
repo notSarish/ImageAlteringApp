75253aed42ea6fe11188358c3ce48c5f9e9676d2
notSarish <sarish.deotale@gmail.com>
2020-04-19 17:38:55 -0400

    got Eigen library cmake files configured

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -3,13 +3,21 @@
 #include "my_app.h"
 
 #include <cinder/app/App.h>
-
+#include <Eigen/Dense>
 
 namespace myapp {
 
 using cinder::app::KeyEvent;
 
-MyApp::MyApp() { }
+MyApp::MyApp() {
+using Eigen::MatrixXd;
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
+}
 
 void MyApp::setup() { }
 

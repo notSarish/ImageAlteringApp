1d791f22fb0d327f91618275a0a674e51607e027
notSarish <sarish.deotale@gmail.com>
2020-04-21 19:42:38 -0400

    Some bugs with opencv going to try to fix them

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -4,19 +4,20 @@
 
 #include <cinder/app/App.h>
 #include <Eigen/Dense>
-#include "opencv2/opencv.hpp"
+#include "CinderOpenCV.h"
+#include "opencv2/core/core.hpp"
+#include <opencv2/highgui/highgui.hpp>
 namespace myapp {
 
 using cinder::app::KeyEvent;
+using namespace cv;
 
 MyApp::MyApp() {
-using Eigen::MatrixXd;
-    MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
-    std::cout << m << std::endl;
+
+    Mat A;
+    std::string file_name = "/Users/sarishdeotale/Downloads/cinder_0.9.2_mac/my-projects/final-project-notSarish/assets/blurryimage.jpg";
+    A = imread(file_name, CV_LOAD_IMAGE_GRAYSCALE);
+   // std::cout << A;
 }
 
 void MyApp::setup() { }
--- src/example.cc
+++ src/example.cc
@@ -1,6 +1,9 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #include <mylibrary/example.h>
+#include <Eigen/Dense>
+#include "opencv2/core/core.hpp"
+#include <opencv2/highgui/highgui.hpp>
 
 namespace mylibrary {
 
